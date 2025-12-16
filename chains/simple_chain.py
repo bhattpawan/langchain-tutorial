478b3d512b25800a24ea0a396b9687a1125ede9a
bhattpawan <bhattpawan1997@gmail.com>
2025-12-16 22:24:37 +0530

    Arranged code into folders

---
+++ chains/simple_chain.py
@@ -0,0 +1,21 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+prompt = PromptTemplate(
+    template="Generate 5 interesting facts about {topic}", input_variables=["topic"]
+)
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+parser = StrOutputParser()
+
+# Langchain expression language
+chain = prompt | model | parser
+
+result = chain.invoke({"topic": "Lionel Messi"})
+
+# chain.get_graph().print_ascii() -> Displays the execution graph of the chain
